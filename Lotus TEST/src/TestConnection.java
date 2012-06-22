2e01acd52d9b578e4d2dad3ab24afa6241a33372
sivanov <sivanov@IVANOV-SN.iba>
2012-06-23 01:52:37 +1300

    Рабочий тест

--- Lotus TEST/src/TestConnection.java
+++ Lotus TEST/src/TestConnection.java
@@ -11,11 +11,20 @@ public class TestConnection
     // TODO Auto-generated function stub
     try
     {
-      Session s = NotesFactory.createSession(args[0], args[1], args[2]);
+      Session session = NotesFactory.createSession(args[0], args[1], args[2]);
       // Operational code goes here
       
-      String server = s.getServerName();
-      System.out.println(server);
+      String server = session.getServerName();
+      System.out.println("Server: " + server);
+      
+      Database db = session.getDatabase(server, args[3]);
+      System.out.println("Database: "+db.getTitle());
+
+      View view = db.getView("All Documents");
+      Document doc = view.getFirstDocument();
+      System.out.println("Document: "+doc.getHttpURL());
+      
+      session.recycle();
     }
     catch (Exception e)
     {
